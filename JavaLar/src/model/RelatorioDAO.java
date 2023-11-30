024e90ca9149e5989130892f2beae9c4da931070
julio <saldanhajuliocesar8@gmail.com>
2023-11-30 17:16:04 -0300

    new adjustments again!

--- JavaLar/src/model/RelatorioDAO.java
+++ JavaLar/src/model/RelatorioDAO.java
@@ -10,10 +10,10 @@ import controller.Relatorio;
 
 public class RelatorioDAO {
 
-
 	public void salvarRelatorio(Relatorio relatorio, Memoria memoria) {
 
-		try (Connection conexao = DriverManager.getConnection(Conexao.getUrl(), Conexao.getUsuario(), Conexao.getSenha())) {
+		try (Connection conexao = DriverManager.getConnection(Conexao.getUrl(), Conexao.getUsuario(),
+				Conexao.getSenha())) {
 
 			String query = "INSERT INTO javalar (nome, matricula, nome_arquivo,bug_python, bug_javascript, bug_ruby, bug_php, bug_csharp, bug_cmais, bug_c,dev_python, dev_javascript, dev_ruby, dev_php, dev_csharp,dev_cmais, dev_c,v_python, v_javascript, v_ruby, v_php, v_csharp, v_cmais, v_c,d_python, d_javascript, d_ruby, d_php, d_csharp,d_cmais,d_c, a_python, a_javascript, a_ruby, a_php, a_csharp, a_cmais, a_c,bug_q1, bug_q2, bug_q3, bug_q4,dev_q1,dev_q2,dev_q3,dev_q4) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
@@ -23,55 +23,19 @@ public class RelatorioDAO {
 				preparedStatement.setString(2, relatorio.getMatricula());
 				preparedStatement.setString(3, relatorio.getNomeArquivo());
 
-				preparedStatement.setInt(4, memoria.getPlanetas().get(0).getBugsCont());
-				preparedStatement.setInt(5, memoria.getPlanetas().get(1).getBugsCont());
-				preparedStatement.setInt(6, memoria.getPlanetas().get(2).getBugsCont());
-				preparedStatement.setInt(7, memoria.getPlanetas().get(3).getBugsCont());
-				preparedStatement.setInt(8, memoria.getPlanetas().get(4).getBugsCont());
-				preparedStatement.setInt(9, memoria.getPlanetas().get(5).getBugsCont());
-				preparedStatement.setInt(10, memoria.getPlanetas().get(6).getBugsCont());
+				enviarChoquesComBugs(memoria, preparedStatement);
 
-				preparedStatement.setInt(11, memoria.getPlanetas().get(0).getDevsCont());
-				preparedStatement.setInt(12, memoria.getPlanetas().get(1).getDevsCont());
-				preparedStatement.setInt(13, memoria.getPlanetas().get(2).getDevsCont());
-				preparedStatement.setInt(14, memoria.getPlanetas().get(3).getDevsCont());
-				preparedStatement.setInt(15, memoria.getPlanetas().get(4).getDevsCont());
-				preparedStatement.setInt(16, memoria.getPlanetas().get(5).getDevsCont());
-				preparedStatement.setInt(17, memoria.getPlanetas().get(6).getDevsCont());
+				enviarChoqueComDevs(memoria, preparedStatement);
 
-				preparedStatement.setInt(18, memoria.getPlanetas().get(0).getVelocidade());
-				preparedStatement.setInt(19, memoria.getPlanetas().get(1).getVelocidade());
-				preparedStatement.setInt(20, memoria.getPlanetas().get(2).getVelocidade());
-				preparedStatement.setInt(21, memoria.getPlanetas().get(3).getVelocidade());
-				preparedStatement.setInt(22, memoria.getPlanetas().get(4).getVelocidade());
-				preparedStatement.setInt(23, memoria.getPlanetas().get(5).getVelocidade());
-				preparedStatement.setInt(24, memoria.getPlanetas().get(6).getVelocidade());
+				enviarVelocidadeDosPlanetas(memoria, preparedStatement);
 
-				preparedStatement.setInt(25, (int) memoria.getPlanetas().get(0).getHoras());
-				preparedStatement.setInt(26, (int) memoria.getPlanetas().get(1).getHoras());
-				preparedStatement.setInt(27, (int) memoria.getPlanetas().get(2).getHoras());
-				preparedStatement.setInt(28, (int) memoria.getPlanetas().get(3).getHoras());
-				preparedStatement.setInt(29, (int) memoria.getPlanetas().get(4).getHoras());
-				preparedStatement.setInt(30, (int) memoria.getPlanetas().get(5).getHoras());
-				preparedStatement.setInt(31, (int) memoria.getPlanetas().get(6).getHoras());
+				enviarHorasDosPlanetas(memoria, preparedStatement);
 
-				preparedStatement.setInt(32, memoria.getPlanetas().get(0).getAnos());
-				preparedStatement.setInt(33, memoria.getPlanetas().get(1).getAnos());
-				preparedStatement.setInt(34, memoria.getPlanetas().get(2).getAnos());
-				preparedStatement.setInt(35, memoria.getPlanetas().get(3).getAnos());
-				preparedStatement.setInt(36, memoria.getPlanetas().get(4).getAnos());
-				preparedStatement.setInt(37, memoria.getPlanetas().get(5).getAnos());
-				preparedStatement.setInt(38, memoria.getPlanetas().get(6).getAnos());
+				enviarAnos(memoria, preparedStatement);
 
-				preparedStatement.setInt(39, relatorio.getBug_q1());
-				preparedStatement.setInt(40, relatorio.getBug_q1());
-				preparedStatement.setInt(41, relatorio.getBug_q1());
-				preparedStatement.setInt(42, relatorio.getBug_q1());
+				enviarQuadrantesBugs(relatorio, preparedStatement);
 
-				preparedStatement.setInt(43, relatorio.getDev_q1());
-				preparedStatement.setInt(44, relatorio.getDev_q2());
-				preparedStatement.setInt(45, relatorio.getDev_q3());
-				preparedStatement.setInt(46, relatorio.getDev_q4());
+				enviarQuadrantesDevs(relatorio, preparedStatement);
 
 				preparedStatement.executeUpdate();
 
@@ -83,6 +47,66 @@ public class RelatorioDAO {
 
 		}
 
+	}
+
+	private void enviarChoquesComBugs(Memoria memoria, PreparedStatement preparedStatement) throws SQLException {
+
+		for (int i = 4; i <= 10; i++) {
+
+			preparedStatement.setInt(i, memoria.getPlanetas().get(i - 4).getBugsCont());
+
+		}
+
+	}
+
+	private void enviarChoqueComDevs(Memoria memoria, PreparedStatement preparedStatement) throws SQLException {
+
+		for (int i = 11; i <= 17; i++) {
+
+			preparedStatement.setInt(i, memoria.getPlanetas().get(i - 11).getDevsCont());
+
+		}
+
+	}
+
+	private void enviarVelocidadeDosPlanetas(Memoria memoria, PreparedStatement preparedStatement) throws SQLException {
+
+		for (int i = 18; i <= 24; i++) {
+
+			preparedStatement.setInt(i, memoria.getPlanetas().get(i - 18).getVelocidade());
+
+		}
+
+	}
+
+	private void enviarHorasDosPlanetas(Memoria memoria, PreparedStatement preparedStatement) throws SQLException {
+
+		for (int i = 25; i <= 31; i++) {
+			preparedStatement.setInt(i, (int) memoria.getPlanetas().get(i - 25).getHoras());
+		}
+
+	}
+
+	private void enviarAnos(Memoria memoria, PreparedStatement preparedStatement) throws SQLException {
+
+		for (int i = 32; i <= 38; i++) {
+			preparedStatement.setInt(i, memoria.getPlanetas().get(i - 32).getAnos());
+		}
+
+	}
+
+	private void enviarQuadrantesBugs(Relatorio relatorio, PreparedStatement preparedStatement) throws SQLException {
+		preparedStatement.setInt(39, relatorio.getBug_q1());
+		preparedStatement.setInt(40, relatorio.getBug_q1());
+		preparedStatement.setInt(41, relatorio.getBug_q1());
+		preparedStatement.setInt(42, relatorio.getBug_q1());
+	}
+
+	private void enviarQuadrantesDevs(Relatorio relatorio, PreparedStatement preparedStatement) throws SQLException {
+		preparedStatement.setInt(43, relatorio.getDev_q1());
+		preparedStatement.setInt(44, relatorio.getDev_q2());
+		preparedStatement.setInt(45, relatorio.getDev_q3());
+		preparedStatement.setInt(46, relatorio.getDev_q4());
 	}
 
 }
--- JavaLar/src/view/PainelDoPlano.java
+++ JavaLar/src/view/PainelDoPlano.java
@@ -171,6 +171,9 @@ public class PainelDoPlano extends JPanel {
 			}
 
 		}
+		
+		
+		
 
 	}
 
